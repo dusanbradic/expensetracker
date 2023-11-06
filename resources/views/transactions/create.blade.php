3f395c95e158e3dddd6d5dacfc6e0c414a0a13a3
dusanbradic <dusanbradic95@gmail.com>
2023-11-06 01:39:05 +0100

    changed the validation error display position

--- resources/views/transactions/create.blade.php
+++ resources/views/transactions/create.blade.php
@@ -1,7 +1,6 @@
-
 @extends('layouts.main')
 @section('content')
-@if ($errors->any())
+<!-- @if ($errors->any())
 <div class="alert alert-danger">
     <ul>
         @foreach ($errors->all() as $error)
@@ -9,7 +8,7 @@
         @endforeach
     </ul>
 </div>
-@endif
+@endif -->
 
 <form method="POST" action="{{ route('transactions.store')}}">
     @csrf
@@ -19,6 +18,9 @@
             Amount
             </label>
             <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" id="amount" name="amount" placeholder="Enter amount" required>
+            @if ($errors->has('amount'))
+            <span class="text-red-500">{{$error}}</span>
+            @endif
             <!-- <p class="text-red-500 text-xs italic">Please fill out this field.</p> -->
         </div>
         <div class="col-span-2 border p-5 hover:font-bold px-60">
@@ -26,6 +28,9 @@
             Category
             </label>
             <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" id="category" name="category" placeholder="Enter category" required>
+            @if ($errors->has('category'))
+            <div>{{$error}}</div>
+            @endif
         </div> 
         <!-- RADIO BUTTONS      -->
 
@@ -40,22 +45,6 @@
         <div class="col-span-2 border hover:font-bold">
             <button class="p-5" type="submit">Add Transaction</button>
         </div>
-        
-
-        <!-- novi -->
-        <!-- <h3 class="col-span-2 mb-5 text-lg font-medium text-center text-gray-900 dark:text-white">Transaction type</h3>
-        <div class="col-span-2 border p-5 hover:font-bold">
-            <input id="income" type="radio" value="1" name="is_income" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-            <label for="income" class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                <div class="radio-label">Income</div>
-            </label>
-        </div>
-        <div class="col-span-2 border p-5 hover:font-bold">
-            <input id="expense" type="radio" value="0" name="is_income" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-            <label for="expense" class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                <div class="radio-label">Expense</div>
-            </label>
-        </div> -->
     </div>
 
 </form>
